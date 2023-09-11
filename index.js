32085ebb3cbc5b3533d89c9ebf2e8d4488668a66
sudhanshudengra <sudhanshudengra.2020cs@technonjr.org>
2023-09-11 18:26:59 +0530

    commit_1

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const server = express();
+const yup = require('yup');
+
+
+const yupObject = yup.object().shape({
+    brand: yup.string().required(),
+    model: yup.string().required(),
+    price: yup.number().required().min(100000).max(10000000),
+    fuel_type: yup.string(),
+    transmission: yup.string().required(),
+    color: yup.string().required(),
+    image: yup.string()
+});
+
+const cars = require('./products.js');
+
+server.use(express.json());
+
+
+server.get('/cars', async (req, res) => {
+    const dbCars = await cars.find()
+    res.send(dbCars);
+});
+
+
+server.get('/cars/:id', async (req, res) => {
+    try {
+        const car = await cars.findOne({ id: req.params.id });
+
+        if (!car) {
+            return res.status(404).send('Car not found');
+        }
+
+        res.status(200).send(car);
+    } catch (error) {
+        // Handle any errors that may occur during the query or response sending.
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+server.post('/cars', (req, res) => {
+   //id: cars.length + 1,
+    const car = req.body
+
+    yupObject.validate(car).then(() => {
+        console.log("Validated");
+        cars.create(car);
+        res.status(201).send("New car added");
+    }).catch((err) => {
+        console.log(err);
+        res.status(400).send("Bad Request");
+    });
+
+});
+
+
+server.patch('/cars/:id', async(req, res) => {
+        const car = await cars.findOne({ id: req.params.id });
+
+    const data = req.body;
+    if (car) {
+        car.price = data.price;
+        res.send("Car updated").status(200);
+    } else {
+        res.status(404).send("Car not found");
+    }
+});
+
+server.delete('/cars/:id', (req, res) => {
+    const car_index = cars.findIndex(car => car.id === parseInt(req.params.id));
+    if (car_index > -1) {
+        cars.splice(car_index, 1);
+        res.send("Car deleted").status(200);
+    } else {
+        res.status(404).send("Car not found");
+    }
+});
+
+server.listen(3000, () => console.log('Listening on port 3000...'));
---
+++ products.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+// const cars = [
+//     {
+//         id: 1,
+//         brand: 'Honda',
+//         model: 'Civic',
+//         price: 1800000,
+//         fuel_type: 'Petrol',
+//         transmission: 'Manual',
+//         color: 'Red',
+//         image: 'https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/ExtraImages/20200709062205_Honda-Civic-BS-6-Diesel.jpg'
+//     },
+//     {
+//         id: 2,
+//         brand: 'BMW',
+//         model: 'X5',
+//         price: 7500000,
+//         fuel_type: 'Diesel',
+//         transmission: 'Automatic',
+//         color: 'Silver',
+//         image: 'https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/ExtraImages/20230714060819_BMW_X5_facelift.jpg'
+//     },
+//     {
+//         id: 3,
+//         brand: 'Audi',
+//         model: 'A8',
+//         price: 1200000,
+//         fuel_type: 'Petrol',
+//         transmission: 'Automatic',
+//         color: 'Black',
+//         image: 'https://www.carblogindia.com/wp-content/uploads/2019/08/2019-audi-a8l-first-drive-1.jpg'
+//     },
+//     {
+//         id: 4,
+//         brand: 'Mercedes',
+//         model: 'C-Class',
+//         price: 6000000,
+//         fuel_type: 'Diesel',
+//         transmission: 'Automatic',
+//         color: 'White',
+//         image: 'https://cdn.cnngreece.gr/media/news/2022/04/28/310330/figure/MERCEDES-AMG-C-43-4MATIC-2.jpg'
+//     }, {
+//         id: 5,
+//         brand: 'Toyota',
+//         model: 'Fortuner',
+//         price: 3500000,
+//         fuel_type: 'Diesel',
+//         transmission: 'Manual',
+//         color: 'Bronze',
+//         image: 'https://blog.gaadikey.com/wp-content/uploads/2018/02/Fortuner-bronze.jpg'
+//     }
+// ]
+const uri = ("mongodb+srv://sudhanshudengra:Dengra8632@cluster0.d5lxdca.mongodb.net/?retryWrites=true&w=majority");
+
+const car_Schema = new mongoose.Schema({
+    id: Number,
+    brand: String,
+    model: String,
+    price: Number,
+    fuel_type: String,
+    transmission: String,
+    color: String,
+    image: String
+});
+
+// const collection = mongoose.model('cars', car_Schema);
+async function connect() {
+    try{
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDB")
+    } catch(err) {
+        console.log(err)
+    }
+}
+// collection.deleteMany({id:1},{id:2},{id:3},{id:4},{id:5});
+connect();
+//  collection.insertMany(cars);
+module.exports = mongoose.model('cars', car_Schema);;
+
